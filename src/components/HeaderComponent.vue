2dfdedc641d809c4fe3f513e4da71139113e8a05
Benson Arafat <benpaul320@gmail.com>
2024-11-25 00:48:05 +0100

    fix menuOpen

--- src/components/HeaderComponent.vue
+++ src/components/HeaderComponent.vue
@@ -1,8 +1,17 @@
 <script lang="ts">
 import { supabase } from '../lib/appsupabase';
+
 export default {
-    name: "HeaderComponent", 
+    name: "HeaderComponent",
+    data() {
+        return {
+            isMenuOpen: false
+        }
+    },
     methods: {
+        toggleMenu() {
+            this.isMenuOpen = !this.isMenuOpen;
+        },
         async signOut() {
             try {
                 const {error} = await supabase.auth.signOut();
@@ -12,11 +21,9 @@ export default {
                 this.$router.push('/')
             } catch (err: any) {
                 console.log('Logout error:', err.message);
-                
             }
         }
     }
-
 }
 </script>
 
@@ -27,28 +34,38 @@ export default {
                 <img src="/logo.png" class="h-8" alt="Flowbite Logo" />
                 <span class="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">FRSC</span>
             </a>
-            <button data-collapse-toggle="navbar-default" type="button" class="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-controls="navbar-default" aria-expanded="false">
+            <button 
+                @click="toggleMenu"
+                type="button" 
+                class="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
+                aria-controls="navbar-default" 
+                :aria-expanded="isMenuOpen"
+            >
                 <span class="sr-only">Open main menu</span>
                 <svg class="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 17 14">
                     <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 1h15M1 7h15M1 13h15"/>
                 </svg>
             </button>
-            <div class="hidden w-full md:block md:w-auto" id="navbar-default">
-            <ul class="font-medium flex flex-col p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 rtl:space-x-reverse md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
-                <li>
-                    <RouterLink to="/dashboard" class='block py-2 px-3 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 dark:text-white md:dark:text-blue-500'>Dashboard</RouterLink>
-                </li>
-                <li>
-                    <RouterLink to="/violation" class='block py-2 px-3 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 dark:text-white md:dark:text-blue-500'>Violations</RouterLink>
-                </li>
-                <li>
-                    <RouterLink to="/product" class='block py-2 px-3 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 dark:text-white md:dark:text-blue-500'>Products</RouterLink>
-                </li>
-                <li>
-                    <a href="javascript:void(0)" @click="signOut" class="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Logout</a>
-                </li>
-            </ul>
+            <div 
+                :class="{ 'hidden': !isMenuOpen }" 
+                class="w-full md:block md:w-auto" 
+                id="navbar-default"
+            >
+                <ul class="font-medium flex flex-col p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 rtl:space-x-reverse md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
+                    <li>
+                        <RouterLink to="/dashboard" class="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0" @click="isMenuOpen = false">Dashboard</RouterLink>
+                    </li>
+                    <li>
+                        <RouterLink to="/violation" class="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0" @click="isMenuOpen = false">Violations</RouterLink>
+                    </li>
+                    <li>
+                        <RouterLink to="/product" class="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0" @click="isMenuOpen = false">Products</RouterLink>
+                    </li>
+                    <li>
+                        <a href="javascript:void(0)" @click="signOut" class="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0">Logout</a>
+                    </li>
+                </ul>
             </div>
         </div>
-        </nav>   
+    </nav>   
 </template>
